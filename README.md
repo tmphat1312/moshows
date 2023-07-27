9e47d93b57be7539c7b7c9131be28268381fe92d
tmphat1312 <tmpsummer@gmail.com>
2023-07-27 13:25:27 +0700

    init app project structure

---
+++ README.md
@@ -0,0 +1,6 @@
+# MoShows - Movies and TV Shows trailers showcase and information
+
+## Note
+This project is still under development.
+
+This README will be updated on the go.
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import logo from "./assets/images/logo-light.svg"
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <img src={logo} alt="logo" />
+    </div>
+  )
+}
+
+export default App
