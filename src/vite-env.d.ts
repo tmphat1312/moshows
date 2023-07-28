185d26bcd2ef9baeff8af8b7c7b4b42161e8a825
tmphat1312 <tmpsummer@gmail.com>
2023-07-28 13:11:14 +0700

    ignored env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_APP_BASE_API: string
+  readonly VITE_TMDB_ACCESS_TOKEN: string
+  readonly VITE_TMDB_TOKEN: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
