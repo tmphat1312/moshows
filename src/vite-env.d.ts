969681b21b6819b86ec7f9ea27b736f9c612d171
tmphat1312 <tmpsummer@gmail.com>
2023-07-29 09:12:00 +0700

    added 1X and 2X image src to make images smaller and responsive

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,8 @@ interface ImportMetaEnv {
   readonly VITE_APP_BASE_API: string
   readonly VITE_TMDB_ACCESS_TOKEN: string
   readonly VITE_TMDB_TOKEN: string
+  readonly VITE_TMDB_IMG_1X_BASE_URL: string
+  readonly VITE_TMDB_IMG_2X_BASE_URL: string
 }
 
 interface ImportMeta {
