3d65fae6a5d4b9d1a7a19973a07672771305a3f7
tmphat1312 <tmpsummer@gmail.com>
2023-07-31 11:14:35 +0700

    added provider card skeleton

--- src/components/ProviderCard.tsx
+++ src/components/ProviderCard.tsx
@@ -1,4 +1,5 @@
 import { APIProviderResults } from "../types/API"
+import { SkeletonBox } from "./Skeleton"
 
 function ProviderCard({ provider }: { provider: APIProviderResults }) {
   return (
@@ -9,6 +10,19 @@ function ProviderCard({ provider }: { provider: APIProviderResults }) {
         className="object-cover w-16 mx-auto border-2 rounded-full md:w-20 aspect-square drop-shadow-lg border-slate-300"
       />
       <h4 className="line-clamp-2 text-balance">{provider.provider_name}</h4>
+    </article>
+  )
+}
+
+export function ProviderCardSkeleton() {
+  return (
+    <article className="w-20 my-4 space-y-2 text-center md:w-16 peer-space-x-sm">
+      <SkeletonBox>
+        <div className="w-16 md:w-20 aspect-square drop-shadow-lg"></div>
+      </SkeletonBox>
+      <SkeletonBox>
+        <h4 className="w-16 h-4 mx-auto"></h4>
+      </SkeletonBox>
     </article>
   )
 }
