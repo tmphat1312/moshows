4e0f705a1f8c7d4cbd2a87466021835b322d7d78
tmphat1312 <tmpsummer@gmail.com>
2023-08-01 14:27:55 +0700

    movie store using zustand

---
+++ src/stores/movieStore.ts
@@ -0,0 +1,87 @@
+import { isAxiosError } from "axios"
+import { create } from "zustand"
+import { authorizedFetcher } from "../services/axios"
+import { APIResponse, APIResponseMovie } from "../types/API"
+
+export const queriesMap = new Map([
+  ["all", "discover/movie"],
+  ["popular", "movie/popular"],
+  ["top rated", "movie/top_rated"],
+  ["upcoming", "movie/upcoming"],
+  ["now playing", "movie/now_playing"],
+])
+export const sortsMap = new Map([
+  ["popularity asc", "popularity.asc"],
+  ["popularity desc", "popularity.desc"],
+  ["revenue asc", "revenue.asc"],
+  ["revenue desc", "revenue.desc"],
+  ["primary release date asc", "primary_release_date.asc"],
+  ["primary release date desc", "primary_release_date.desc"],
+  ["vote average asc", "vote_average.asc"],
+  ["vote average desc", "vote_average.desc"],
+])
+
+export const queries = Array.from(queriesMap.keys())
+export const sorts = Array.from(sortsMap.keys())
+export type Query = (typeof queries)[number]
+export type Sort = (typeof sorts)[number]
+
+export type MovieState = {
+  movies: APIResponseMovie[]
+  movieTypeQuery: Query
+  sortQuery: Sort
+  status: "pending" | "resolved" | "rejected" | "idle"
+  error: null | Error
+  setQuery: (query: Query) => void
+  setSort: (sort: Sort) => void
+  getMovies: () => void
+}
+
+export const useMovieStore = create<MovieState>()((set, get) => ({
+  movies: [],
+  movieTypeQuery: "discover/movie",
+  sortQuery: "popularity.desc",
+  status: "idle",
+  error: null,
+  setQuery: (query) => {
+    const { getMovies } = get()
+    const mappedQuery = queriesMap.get(query)
+    set({ movieTypeQuery: mappedQuery })
+    getMovies()
+  },
+  setSort: (sort) => {
+    const { getMovies } = get()
+    const mappedSort = sortsMap.get(sort)
+    set({ sortQuery: mappedSort })
+    getMovies()
+  },
+  getMovies: async () => {
+    const { movieTypeQuery } = get()
+    const BASE_URL = import.meta.env.VITE_APP_BASE_API
+
+    set({ status: "pending" })
+
+    try {
+      const response = await authorizedFetcher.get<
+        APIResponse<APIResponseMovie>
+      >(`${BASE_URL}/${movieTypeQuery}`)
+
+      set({ movies: response.data.results, status: "resolved" })
+    } catch (error) {
+      let customError
+      if (isAxiosError(error)) {
+        customError = new Error(error.response?.data.status_message)
+      } else if (error instanceof Error) {
+        customError = error
+      } else {
+        customError = new Error(
+          "Unknown error: Something went wrong! @movieStore"
+        )
+      }
+
+      console.error(error)
+
+      set({ error: customError, status: "rejected" })
+    }
+  },
+}))
