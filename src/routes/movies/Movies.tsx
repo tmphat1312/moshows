a42eb45e6a86f64b31e63b8fa7e26449f64957c9
tmphat1312 <tmpsummer@gmail.com>
2023-08-01 14:27:35 +0700

    movie page structure

--- src/routes/movies/Movies.tsx
+++ src/routes/movies/Movies.tsx
@@ -1,5 +1,8 @@
 import NavBar from "../../layout/NavBar"
 import TitleSection from "../../layout/TitleSection"
+import MovieActions from "./MovieActions"
+import MovieGridView from "./MovieGridView"
+import MovieNav from "./MovieNav"
 
 function Movies() {
   return (
@@ -9,8 +12,16 @@ function Movies() {
       </div>
       <div className="section-separator">
         <TitleSection title="Movies" />
+        <MovieNav />
+        <div className="flex gap-8 app-width section">
+          <div className="basis-1/5 shrink-0">
+            <MovieActions />
+          </div>
+          <div className="grow">
+            <MovieGridView />
+          </div>
+        </div>
       </div>
-      <div className="h-[2000px] section-separator"></div>
     </>
   )
 }
