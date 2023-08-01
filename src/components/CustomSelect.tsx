957216b186408ba564be95d815313f9d43951860
tmphat1312 <tmpsummer@gmail.com>
2023-08-01 12:57:39 +0700

    fixed not unique key

--- src/components/CustomSelect.tsx
+++ src/components/CustomSelect.tsx
@@ -2,8 +2,8 @@ function CustomSelect({ items }: CustomSelectProps) {
   return (
     <select className="w-full px-4 py-1 text-base text-gray-900 border border-gray-400 rounded-md shadow-sm sm:hidden bg-slate-400 hover:border-gray-500">
       {items.map((item) => (
-        <option key={item.value} value={item.value}>
-          {item.string}
+        <option key={item.value + item.text} value={item.value}>
+          {item.text}
         </option>
       ))}
     </select>
@@ -12,7 +12,7 @@ function CustomSelect({ items }: CustomSelectProps) {
 
 export type CustomSelectProps = {
   items: {
-    string: string | number
+    text: string
     value: string | number
   }[]
 }
