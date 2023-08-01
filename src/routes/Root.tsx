02b1bdca98e0e8bd59a7040ef9fc5bc2199ae27d
tmphat1312 <tmpsummer@gmail.com>
2023-08-01 09:52:27 +0700

    moved footer to root route for common layout

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Header from "../layout/Header"
+import Footer from "../layout/Footer"
 
 function Root() {
   return (
@@ -8,6 +9,9 @@ function Root() {
       <main>
         <Outlet />
       </main>
+      <div className="section-separator">
+        <Footer />
+      </div>
     </>
   )
 }
