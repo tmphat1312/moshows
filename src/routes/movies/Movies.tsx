94bfdd43e24944c958c766264e691839ad6dc018
tmphat1312 <tmpsummer@gmail.com>
2023-08-01 17:53:52 +0700

    made fitler only work for all movies first, Ill change it later

--- src/routes/movies/Movies.tsx
+++ src/routes/movies/Movies.tsx
@@ -1,10 +1,13 @@
 import NavBar from "../../layout/NavBar"
 import TitleSection from "../../layout/TitleSection"
+import { useMovieStore } from "../../stores/movieStore"
 import MovieActions from "./MovieActions"
 import MovieGridView from "./MovieGridView"
 import MovieNav from "./MovieNav"
 
 function Movies() {
+  const query = useMovieStore((state) => state.movieTypeQuery)
+
   return (
     <>
       <div className="invisible">
@@ -14,9 +17,11 @@ function Movies() {
         <TitleSection title="Movies" />
         <MovieNav />
         <div className="relative flex gap-8 app-width section">
-          <div className="basis-1/5 shrink-0">
-            <MovieActions />
-          </div>
+          {query == "discover/movie" && (
+            <div className="basis-1/5 shrink-0">
+              <MovieActions />
+            </div>
+          )}
           <div className="grow">
             <MovieGridView />
           </div>
