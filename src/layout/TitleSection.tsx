4a4e9523682888eef80fc322238872a576c5aabb
tmphat1312 <tmpsummer@gmail.com>
2023-08-01 10:01:08 +0700

    added title section

---
+++ src/layout/TitleSection.tsx
@@ -0,0 +1,9 @@
+function TitleSection({ title }: { title: string }) {
+  return (
+    <section className="title-section">
+      <h2 className="text-center title">{title}</h2>
+    </section>
+  )
+}
+
+export default TitleSection
--- src/routes/movies/Movies.tsx
+++ src/routes/movies/Movies.tsx
@@ -1,5 +1,18 @@
+import NavBar from "../../layout/NavBar"
+import TitleSection from "../../layout/TitleSection"
+
 function Movies() {
-  return <div>Movies</div>
+  return (
+    <>
+      <div className="invisible">
+        <NavBar />
+      </div>
+      <div className="section-separator">
+        <TitleSection title="Movies" />
+      </div>
+      <div className="h-[2000px] section-separator"></div>
+    </>
+  )
 }
 
 export default Movies
