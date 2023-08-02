fe08ead9e1c3379a3217654483b7cccbf8800a8b
tmphat1312 <tmpsummer@gmail.com>
2023-08-02 18:08:01 +0700

    itemcard and video card not work well on my phone, so I made it better :))

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -13,7 +13,7 @@ function VideoCard({ item }: { item: APIResults }) {
   const title = item.media_type == "movie" ? item.title : item.name
 
   return (
-    <article className="w-64 py-4 space-y-3 text-center md:w-72">
+    <article className="py-4 space-y-3 text-center w-52 sm:w-64 md:w-72">
       <div className="relative aspect-video">
         {item.backdrop_path ? (
           <Link to={`/${item.media_type}/${item.id}`}>
@@ -61,7 +61,7 @@ function VideoCard({ item }: { item: APIResults }) {
 
 export function VideoCardSkeleton() {
   return (
-    <article className="w-64 py-4 space-y-3 text-center md:w-72">
+    <article className="py-4 space-y-3 text-center w-52 sm:w-64 md:w-72">
       <SkeletonBox>
         <div className="rounded-lg aspect-video" />
       </SkeletonBox>
