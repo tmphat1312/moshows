59f7a8785422331c554638ffca9a1bba314d31a3
tmphat1312 <tmpsummer@gmail.com>
2023-08-02 10:20:48 +0700

    added default value to the custom select

--- src/components/CustomSelect.tsx
+++ src/components/CustomSelect.tsx
@@ -1,8 +1,9 @@
-function CustomSelect({ items, action }: CustomSelectProps) {
+function CustomSelect({ items, action, defaultValue }: CustomSelectProps) {
   return (
     <select
       className="w-full px-4 py-1 text-base text-gray-900 capitalize border border-gray-400 rounded-md shadow-sm bg-slate-400 hover:border-gray-500"
       onChange={(e) => action(e.target.value)}
+      defaultValue={defaultValue}
     >
       {items.map((item) => (
         <option key={item.value + item.text} value={item.value}>
@@ -19,6 +20,7 @@ export type CustomSelectProps = {
     value: string
   }[]
   action: (value: string) => void
+  defaultValue?: string
 }
 
 export default CustomSelect
