0cbb53509b9ddf0b16406305627b8b36417a1f92
tmphat1312 <tmpsummer@gmail.com>
2023-08-02 13:42:24 +0700

    made sort and filter available to all types of movie

--- src/routes/movies/Movies.tsx
+++ src/routes/movies/Movies.tsx
@@ -1,13 +1,10 @@
 import NavBar from "../../layout/NavBar"
 import TitleSection from "../../layout/TitleSection"
-import { useMovieStore } from "../../stores/movieStore"
 import MovieActions from "./MovieActions"
 import MovieGridView from "./MovieGridView"
 import MovieNav from "./MovieNav"
 
 function Movies() {
-  const query = useMovieStore((state) => state.movieTypeQuery)
-
   return (
     <>
       <div className="invisible">
@@ -17,11 +14,9 @@ function Movies() {
         <TitleSection title="Movies" />
         <MovieNav />
         <div className="relative flex gap-8 app-width section">
-          {query == "discover/movie" && (
-            <div className="basis-1/5 shrink-0">
-              <MovieActions />
-            </div>
-          )}
+          <div className="basis-1/5 shrink-0">
+            <MovieActions />
+          </div>
           <div className="grow">
             <MovieGridView />
           </div>
