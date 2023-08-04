43d0a33338eb25339c8ae8d01f7de7f52a190333
tmphat1312 <tmpsummer@gmail.com>
2023-08-04 13:50:29 +0700

    added single movie result and single tv show result type

--- src/types/API.ts
+++ src/types/API.ts
@@ -78,3 +78,89 @@ export type APIGenreResults = {
   id: number
   name: string
 }
+
+export type APISingleMovieResult = {
+  adult: boolean
+  backdrop_path: string
+  belongs_to_collection: null
+  budget: number
+  genres: APIGenreResults[]
+  homepage: string
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  revenue: number
+  runtime: number
+  status: string
+  tagline: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+export type APISingleTVResult = {
+  adult: boolean
+  backdrop_path: string
+  episode_run_time: number[]
+  first_air_date: string
+  genres: APIGenreResults[]
+  homepage: string
+  id: number
+  last_air_date: string
+  last_episode_to_air: {
+    id: number
+    name: string
+    overview: string
+    vote_average: number
+    vote_count: number
+    air_date: string
+    episode_number: number
+    production_code: string
+    runtime: number | null
+    season_number: number
+    show_id: number
+    still_path: string | null
+  }
+  name: string
+  next_episode_to_air: {
+    id: number
+    name: string
+    overview: string
+    vote_average: number
+    vote_count: number
+    air_date: string
+    episode_number: number
+    production_code: string
+    runtime: number | null
+    season_number: number
+    show_id: number
+    still_path: string | null
+  }
+  number_of_episodes: number
+  number_of_seasons: number
+  original_language: string
+  original_name: string
+  overview: string
+  popularity: number
+  poster_path: string
+  seasons: {
+    air_date: string
+    episode_count: number
+    id: number
+    name: string
+    overview: string
+    poster_path: string | null
+    season_number: number
+    vote_average: number
+  }[]
+  status: string
+  tagline: string
+  type: string
+  vote_average: number
+  vote_count: number
+}
