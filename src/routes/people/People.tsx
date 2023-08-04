9766e73c6650fe90ef52fb07ab17075f7d5fc19b
tmphat1312 <tmpsummer@gmail.com>
2023-08-04 09:41:44 +0700

    popular people page implemented

--- src/routes/people/People.tsx
+++ src/routes/people/People.tsx
@@ -1,5 +1,21 @@
+import NavBar from "../../layout/NavBar"
+import TitleSection from "../../layout/TitleSection"
+import PeopleView from "./PeopleView"
+
 function People() {
-  return <div>People</div>
+  return (
+    <>
+      <div className="invisible">
+        <NavBar />
+      </div>
+      <div className="section-separator">
+        <TitleSection title="Popular people" />
+        <div className="app-width">
+          <PeopleView />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default People
