b47cf61aa75637c5072d125284510e9b0cc8db67
tmphat1312 <tmpsummer@gmail.com>
2023-08-05 09:21:18 +0700

    added no image placholder

---
+++ src/components/NoImage.tsx
@@ -0,0 +1,11 @@
+import noImage from "../assets/images/no-image.svg"
+
+function NoImage() {
+  return (
+    <div className="w-full">
+      <img src={noImage} alt="No image" />
+    </div>
+  )
+}
+
+export default NoImage
