a1799613f656f007aacb8a2d53f6e26a8c2b8ce3
tmphat1312 <tmpsummer@gmail.com>
2023-08-05 18:18:02 +0700

    refactored structure and handle error, no items found

--- src/routes/single-showcase/Recommendation.tsx
+++ src/routes/single-showcase/Recommendation.tsx
@@ -1,10 +1,10 @@
 import { useParams } from "react-router-dom"
+import BackgroundWall from "../../components/BackgroundWall"
 import CommonErrorMessage from "../../components/CommonErrorMessage"
 import CustomScrollingCarousel from "../../components/CustomScrollingCarousel"
 import ItemCard, { ItemCardSkeleton } from "../../components/ItemCard"
 import { useFetch } from "../../hooks/useFetch"
 import { APIResponse, APIResults } from "../../types/API"
-import BackgroundWall from "../../components/BackgroundWall"
 
 function Recommendation() {
   const { id, type } = useParams<{ id: string; type: string }>()
@@ -15,38 +15,46 @@ function Recommendation() {
   if (error) {
     return (
       <section>
-        <h2 className="title">Recommendation</h2>
-        <CommonErrorMessage />
+        <BackgroundWall>
+          <h2 className="title">Recommendation</h2>
+          <CommonErrorMessage />
+        </BackgroundWall>
       </section>
     )
   }
 
-  const carouselContent =
-    status == "pending" || !data ? (
+  if (status == "pending") {
+    return (
+      <section>
+        <h2 className="title">Recommendation</h2>
+        <BackgroundWall>
+          <CustomScrollingCarousel>
+            {Array.from({ length: 7 }).map((_, index) => (
+              <ItemCardSkeleton key={index} />
+            ))}
+          </CustomScrollingCarousel>
+        </BackgroundWall>
+      </section>
+    )
+  }
+
+  const recommendationItems = data?.results ?? []
+  const content =
+    recommendationItems.length > 0 ? (
       <CustomScrollingCarousel>
-        <ItemCardSkeleton />
-        <ItemCardSkeleton />
-        <ItemCardSkeleton />
-        <ItemCardSkeleton />
-        <ItemCardSkeleton />
-        <ItemCardSkeleton />
-        <ItemCardSkeleton />
-      </CustomScrollingCarousel>
-    ) : (
-      <CustomScrollingCarousel>
-        {data.results.map((item) => (
+        {recommendationItems.map((item) => (
           <ItemCard key={item.id} item={item} type={type as "tv" | "movie"} />
         ))}
       </CustomScrollingCarousel>
+    ) : (
+      <p className="my-6 text-2xl font-display">No items available</p>
     )
-
-  // TODO: NO videos found
 
   return (
     <section>
       <BackgroundWall>
         <h2 className="title">Recommendation</h2>
-        {carouselContent}
+        {content}
       </BackgroundWall>
     </section>
   )
--- src/routes/single-showcase/Similar.tsx
+++ src/routes/single-showcase/Similar.tsx
@@ -14,39 +14,42 @@ function Similar() {
   if (error) {
     return (
       <section className="section">
-        <div className="flex-btw">
-          <h2 className="title">Similar</h2>
-        </div>
+        <h2 className="title">Similar</h2>
         <CommonErrorMessage />
       </section>
     )
   }
 
-  const carouselContent =
-    status == "pending" || !data ? (
+  if (status == "pending") {
+    return (
+      <section className="section">
+        <h2 className="title">Similar</h2>
+        <CustomScrollingCarousel>
+          {Array.from({ length: 7 }).map((_, index) => (
+            <ItemCardSkeleton key={index} />
+          ))}
+          <ItemCardSkeleton />
+        </CustomScrollingCarousel>
+      </section>
+    )
+  }
+
+  const similarItems = data?.results ?? []
+  const content =
+    similarItems.length > 0 ? (
       <CustomScrollingCarousel>
-        <ItemCardSkeleton />
-        <ItemCardSkeleton />
-        <ItemCardSkeleton />
-        <ItemCardSkeleton />
-        <ItemCardSkeleton />
-        <ItemCardSkeleton />
-        <ItemCardSkeleton />
-      </CustomScrollingCarousel>
-    ) : (
-      <CustomScrollingCarousel>
-        {data.results.map((item) => (
+        {similarItems.map((item) => (
           <ItemCard key={item.id} item={item} type={type as "tv" | "movie"} />
         ))}
       </CustomScrollingCarousel>
+    ) : (
+      <p className="my-6 text-2xl font-display">No items available</p>
     )
-
-  // TODO: NO videos found
 
   return (
     <section>
       <h2 className="title">Similar</h2>
-      <div>{carouselContent}</div>
+      <div>{content}</div>
     </section>
   )
 }
