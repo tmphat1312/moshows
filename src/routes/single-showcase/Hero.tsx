1b8f1f5381d73fa2c015f3984a915bc6103d7b96
tmphat1312 <tmpsummer@gmail.com>
2023-08-05 09:26:12 +0700

    hero structured

---
+++ src/routes/single-showcase/Hero.tsx
@@ -0,0 +1,39 @@
+import { APISingleMovieResult } from "../../types/API"
+import Credits from "./Credits"
+import Info from "./Info"
+import Poster from "./Poster"
+
+const BASE_URL = import.meta.env.VITE_TMDB_HERO_BASE_URL
+
+function Hero({ item }: HeroProps) {
+  return (
+    <div
+      className="bg-slate-900"
+      style={{
+        backgroundImage: `url(${BASE_URL}${item.backdrop_path})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
+        backgroundBlendMode: "overlay",
+      }}
+    >
+      <div className="app-width">
+        <section className="flex flex-col gap-8 py-8 sm:flex-row sm:items-center backdrop-blur-sm">
+          <div className="shrink-0">
+            <Poster adult={item.adult} posterPath={item.poster_path} />
+          </div>
+          <div>
+            <Info item={item} />
+          </div>
+        </section>
+        <Credits />
+      </div>
+    </div>
+  )
+}
+
+export type HeroProps = {
+  item: APISingleMovieResult
+}
+
+export default Hero
