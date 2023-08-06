bb9aa03f233baca2f442756e438105c9126110e0
tmphat1312 <tmpsummer@gmail.com>
2023-08-06 14:39:49 +0700

    added more info

--- src/routes/person/Info.tsx
+++ src/routes/person/Info.tsx
@@ -3,9 +3,11 @@ import { APISinglePersonResult } from "../../types/API"
 function Info({ item }: InfoProps) {
   return (
     <section className="space-y-4">
-      <h2 className="text-3xl">{item.name}</h2>
+      <h2 className="text-4xl">{item.name}</h2>
       <section className="space-y-2">
-        <h4 className="text-xl">Biography</h4>
+        <h4 className="px-1 text-xl rounded-sm bg-gradient-to-r w-max from-red-600 to-red-700">
+          Biography
+        </h4>
         <p className="tracking-wider text-balance">
           {item.biography.length > 0 ? (
             item.biography
@@ -15,6 +17,18 @@ function Info({ item }: InfoProps) {
             </>
           )}
         </p>
+      </section>
+      <section className="space-y-2">
+        <h4 className="px-1 text-xl rounded-sm bg-gradient-to-r w-max from-red-600 to-red-700">
+          Date of birth
+        </h4>
+        <p>{item.birthday}</p>
+      </section>
+      <section className="space-y-2">
+        <h4 className="px-1 text-xl rounded-sm bg-gradient-to-r w-max from-red-600 to-red-700">
+          Place of birth
+        </h4>
+        <p>{item.place_of_birth}</p>
       </section>
     </section>
   )
