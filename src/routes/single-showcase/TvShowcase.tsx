9db47f2f7baa4f06282aa17857c764b2628b4b98
tmphat1312 <tmpsummer@gmail.com>
2023-08-06 09:58:18 +0700

    hero section of tv showcase

--- src/routes/single-showcase/TvShowcase.tsx
+++ src/routes/single-showcase/TvShowcase.tsx
@@ -1,8 +1,15 @@
 import { APISingleTVResult } from "../../types/API"
+import Hero from "./Hero"
 
 function TvShowcase({ data }: SingleShowCaseProps) {
   console.log(data)
-  return <div>TvShowcase</div>
+  return (
+    <>
+      <div className="section-separator">
+        <Hero item={data} />
+      </div>
+    </>
+  )
 }
 
 export type SingleShowCaseProps = {
