8716d4b8a136ae6e0cf54c450dfd9ac5442c288e
tmphat1312 <tmpsummer@gmail.com>
2023-08-06 09:37:47 +0700

    render movie or tv single showcase page conditionally

--- src/routes/single-showcase/Hero.tsx
+++ src/routes/single-showcase/Hero.tsx
@@ -1,11 +1,22 @@
-import { APISingleMovieResult } from "../../types/API"
+import { useParams } from "react-router-dom"
+import { APISingleMovieResult, APISingleTVResult } from "../../types/API"
 import Credits from "./Credits"
-import Info from "./Info"
+import MovieInfo from "./MovieInfo"
 import Poster from "./Poster"
+import TvInfo from "./TvInfo"
 
 const BASE_URL = import.meta.env.VITE_TMDB_HERO_BASE_URL
 
 function Hero({ item }: HeroProps) {
+  const { type } = useParams<{ type: string }>()
+
+  const info =
+    type === "tv" ? (
+      <TvInfo item={item as APISingleTVResult} />
+    ) : (
+      <MovieInfo item={item as APISingleMovieResult} />
+    )
+
   return (
     <div
       className="bg-slate-900"
@@ -22,9 +33,7 @@ function Hero({ item }: HeroProps) {
           <div className="shrink-0">
             <Poster adult={item.adult} posterPath={item.poster_path} />
           </div>
-          <div>
-            <Info item={item} />
-          </div>
+          <div>{info}</div>
         </section>
         <Credits />
       </div>
@@ -33,7 +42,7 @@ function Hero({ item }: HeroProps) {
 }
 
 export type HeroProps = {
-  item: APISingleMovieResult
+  item: APISingleMovieResult | APISingleTVResult
 }
 
 export default Hero
