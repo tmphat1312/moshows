325f8f551707d077a9deb4981df3b4f336dcc956
tmphat1312 <tmpsummer@gmail.com>
2023-08-07 13:08:04 +0700

    padded no word

--- src/components/NoItemsMessage.tsx
+++ src/components/NoItemsMessage.tsx
@@ -1,7 +1,7 @@
 function NoItemsMessage() {
   return (
     <h5 className="my-6 text-2xl">
-      <b className="bg-red-500">No</b> items available
+      <b className="px-1 bg-red-500 rounded-sm">No</b> items available
     </h5>
   )
 }
