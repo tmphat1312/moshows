8630778cdc755dcfe5f7543432c96897c3652d88
tmphat1312 <tmpsummer@gmail.com>
2023-08-07 13:01:44 +0700

    removed unused tailwindcss class and made padding smaller in small screens

--- src/components/BackgroundWall.tsx
+++ src/components/BackgroundWall.tsx
@@ -1,6 +1,6 @@
 function BackgroundWall({ children }: BackgroundWallProps) {
   return (
-    <div className="relative p-6 overflow-hidden rounded-md bg-gradient-to-r from-primary-200/10 to-slate-700/30">
+    <div className="p-4 rounded-md md:p-6 bg-gradient-to-r from-primary-200/10 to-slate-700/30">
       {children}
     </div>
   )
