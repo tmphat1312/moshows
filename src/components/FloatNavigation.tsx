706ea38fb96f31a7a98a10dd79439f62407ca111
tmphat1312 <tmpsummer@gmail.com>
2023-08-09 11:08:43 +0700

    cool float navigation with border radius

--- src/components/FloatNavigation.tsx
+++ src/components/FloatNavigation.tsx
@@ -4,21 +4,22 @@ import { navigationLinks } from "../constants"
 
 function Navigation() {
   return (
-    <nav className="absolute right-[4%] flex flex-col gap-4 text-xl font-medium capitalize top-[120%] text-center">
+    <nav className="absolute right-[4%] flex flex-col gap-4 top-[120%] text-center drop-shadow-lg p-8 rounded-md cool-radius bg-gradient-to-tr from-slate-500 to-slate-700">
       {navigationLinks.map((link) => (
         <NavLink
           key={link.name}
           to={link.path}
           className={({ isActive }) =>
             clsx(
-              "px-4 py-2 rounded-md bg-slate-600",
-              isActive && "text-primary-500 underline"
+              "px-4 py-2 rounded-md bg-slate-700 text-xl font-medium capitalize",
+              isActive && "text-primary-500 overline"
             )
           }
         >
           {link.name}
         </NavLink>
       ))}
+      <div className="absolute inset-y-8 bg-primary-500 inset-x-[48%] -z-10" />
     </nav>
   )
 }
