f70800a1f0074fb39affdd793d3d1241f1fabec7
tmphat1312 <tmpsummer@gmail.com>
2023-08-09 10:23:04 +0700

    changed from anchor tag to react router Link component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,16 +1,17 @@
 import { BiMoviePlay } from "react-icons/bi"
+import { Link } from "react-router-dom"
 
 function Logo() {
   return (
-    <a
-      href="/"
+    <Link
+      to="/"
       className="flex items-center text-[0.8em] sm:text-[1em] font-display text-gradient-primary"
     >
       <span className="translate-y-[1px] text-primary-500">
         <BiMoviePlay />
       </span>
       MoShows
-    </a>
+    </Link>
   )
 }
 
